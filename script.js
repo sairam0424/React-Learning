53842d036f22251b54de7c44f2ce7008c2e4ae68
sairam0424 <uggesairam0000@gmail.com>
2024-07-30 00:22:07 +0530

    Added parent-child DIv using React

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const heading = React.createElement("h1",{id:"header"},"Hello World From React");
+// const heading = React.createElement("h1",{id:"header"},"Hello World From React");
+
+
+const parent = React.createElement("div",{id:"parent"},[
+
+    React.createElement("div",{id:"child"},[
+
+    React.createElement("h1",{id:"child1"},"I am Child 1"),
+
+    React.createElement("h1",{id:"child2"},"I am child 2")]),
+
+    React.createElement("div",{id:"NextChild"},[
+
+        React.createElement("h1",{id:"child1"},"I am Nextchild1"),
+
+        React.createElement("h1",{id:"child2"},"I am Nextchild2")
+    ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
 
